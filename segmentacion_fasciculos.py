625fd41feefd9c08861b6241fcd5b97a4628e310
Matías Medina <matias.medina.s2@gmail.com>
2018-06-15 10:44:30 -0400

    10.000 size of data

--- segmentacion_fasciculos.py
+++ segmentacion_fasciculos.py
@@ -23,67 +23,56 @@ for root, dirs, files in os.walk(atlas_path):
             fasciculos_atlas.append(fasciculo)
 
 whole_brain = read_bundle(brain_path)
-whole_brain_chunks = list(chunks(whole_brain, 10000))
-umbral = 9
+n_threads = 10
+chunk = 100000 / n_threads
+whole_brain_chunks = list(chunks(whole_brain, int(chunk)))
+umbral = 10
 
 def distance(id):
+    count = 0
     brain_chunk = whole_brain_chunks[id]
-    for fasciculo_atlas in fasciculos_atlas:
-        for fibra_atlas in fasciculo_atlas:
-            for index,fibra_datos in enumerate(brain_chunk):
+    for fibra_datos in brain_chunk:
+        brain_data_exit = False
+        for fasciculo_atlas in fasciculos_atlas:
+            for fibra_atlas in fasciculo_atlas:
                 umbral_exit = False
                 max_1 = 0
                 max_2 = 0
-                n = len(fibra_datos)
-                for i in range(0, n):
+                for i in range(0, 21):
                     a = fibra_atlas[i]
                     b_1 = fibra_datos[i]
-                    b_2 = fibra_datos[n - 1 - i]
+                    b_2 = fibra_datos[20 - i]
                     dist_1 = numpy.linalg.norm(a - b_1)
                     dist_2 = numpy.linalg.norm(a - b_2)
                     if dist_1 > max_1:
                         max_1 = dist_1
                     if dist_2 > max_2:
                         max_2 = dist_2
-                    if min(max_1,max_2) > umbral:
+                    if dist_1 and dist_2 > umbral:
                         umbral_exit = True
                         break
                 if umbral_exit:
                     continue
                 dist = min(max_1, max_2)
                 if dist < umbral:
-                    brain_chunk.pop(index)
-                    print("ok")
+                    brain_data_exit = True
+                    count+=1
                     break
+            if brain_data_exit:
+                break
+        if brain_data_exit:
+            continue
+    print('id: {}, count {}'.format(id,count))
 
 class myThread(threading.Thread):
     def __init__(self, threadID):
         threading.Thread.__init__(self)
         self.threadID = threadID
+        self.count = 0
 
     def run(self):
-        distance(self.threadID - 1)
+        distance(self.threadID)
 
-# Create new threads
-thread1 = myThread(1)
-thread2 = myThread(2)
-thread3 = myThread(3)
-thread4 = myThread(4)
-thread5 = myThread(5)
-thread6 = myThread(6)
-thread7 = myThread(7)
-thread8 = myThread(8)
-thread9 = myThread(9)
-thread10 = myThread(10)
-
-# Start new Threads
-thread1.start()
-thread2.start()
-thread3.start()
-thread4.start()
-thread5.start()
-thread6.start()
-thread7.start()
-thread8.start()
-thread9.start()
-thread10.start()
+myThread(0).start()
+'''for i in range(0,n_threads):
+    myThread(i).start()'''
